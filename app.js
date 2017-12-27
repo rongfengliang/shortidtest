4df9258d0c3fbee5ac2ea0ad75320cd5f514d719
rongfengliang <rongfengliang@k8s.net.cn>
2017-12-27 10:33:59 +0800

    add readme file

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const shortid = require("shortid");
+
+
+var index =0;
+
+var list=[];
+for(var i=0;i<100000000000;i++) {
+
+  list.push(shortid.generate());
+  index++;
+  console.log("the count is",index,"the insert count is:",list.length);
+  if (list.length!=index) {
+   console.log("there is some key is repeated please checj it")
+ }
+}
